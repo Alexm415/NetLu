ef0def8d3e9c923532230af3e68163128cbbb196
Arman Tork <canadienperse@gmail.com>
2024-09-17 20:18:32 -0700

    added some test js

--- assest/js/homepage.js
+++ assest/js/homepage.js
@@ -1,6 +1,7 @@
 let tmdbApiKey = "aeaff9aa6a2dfe6e52db45e5c6947320";
 let urlTopTMDB = `https://api.themoviedb.org/3/movie/top_rated?api_key=${tmdbApiKey}`;
 let urlPlayingTMDB = `https://api.themoviedb.org/3/movie/now_playing?api_key=${tmdbApiKey}`;
+let containerEl = document.getElementById('container');
 
 function top_rated() {
   fetch(urlTopTMDB)
@@ -22,5 +23,11 @@ function now_playing() {
     });
 }
 
-top_rated();
-now_playing();
+let ratedList = JSON.parse(top_rated());
+let playingList = JSON.parse(now_playing());
+
+for (let i = 0; i < ratedList.length; i++){
+    let movieEl = document.createElement('div');
+    movieEl.textContent = ratedList[i].title;
+    containerEl.appendChild(movieEl);
+}
