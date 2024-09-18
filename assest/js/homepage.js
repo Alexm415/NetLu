eaf83c80a1319e45b5ce90de1a81df815a1175ac
Arman Tork <canadienperse@gmail.com>
2024-09-17 20:40:11 -0700

    modified js

--- assest/js/homepage.js
+++ assest/js/homepage.js
@@ -1,7 +1,7 @@
 let tmdbApiKey = "aeaff9aa6a2dfe6e52db45e5c6947320";
 let urlTopTMDB = `https://api.themoviedb.org/3/movie/top_rated?api_key=${tmdbApiKey}`;
 let urlPlayingTMDB = `https://api.themoviedb.org/3/movie/now_playing?api_key=${tmdbApiKey}`;
-let containerEl = document.getElementById('container');
+let containerEl = document.getElementById("container");
 
 function top_rated() {
   fetch(urlTopTMDB)
@@ -10,11 +10,18 @@ function top_rated() {
     })
     .then(function (data) {
       console.log(data);
+
+      for (let i = 0; i < data.results.length; i++) {
+        let movieEl = document.createElement("div");
+        movieEl.textContent = data.results[i].title;
+        console.log(data.results[i].title);
+        containerEl.appendChild(movieEl);
+      }
     });
 }
 
 function now_playing() {
-    fetch(urlPlayingTMDB)
+  fetch(urlPlayingTMDB)
     .then(function (response) {
       return response.json();
     })
@@ -23,11 +30,4 @@ function now_playing() {
     });
 }
 
-let ratedList = JSON.parse(top_rated());
-let playingList = JSON.parse(now_playing());
-
-for (let i = 0; i < ratedList.length; i++){
-    let movieEl = document.createElement('div');
-    movieEl.textContent = ratedList[i].title;
-    containerEl.appendChild(movieEl);
-}
+top_rated();
