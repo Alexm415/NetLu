489ad8ad55ad6df15777f68d5a6efdc0be3ef866
Alexander mena <alexismena-90@hotmail.com>
2024-09-17 20:45:24 -0700

    newcom

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,17 @@
 const toprated = document.querySelector(".toprated");
+const featured = document.querySelector(".featured");
+const theater = document.querySelector(".in-theater");
 
 toprated.addEventListener("click", function (event) {
   event.preventDefault();
   window.location = "toprated.html";
+});
+
+featured.addEventListener("click", function (event) {
+  event.preventDefault();
+  window.location = "featured.html";
+});
+theater.addEventListener("click", function (event) {
+  event.preventDefault();
+  window.location = "intheaters.html";
 });
--- assets/js/toprated.js
+++ assets/js/toprated.js
@@ -1,7 +1,19 @@
 const toprated = document.querySelector(".toprated");
 const featured = document.querySelector(".featured");
-const theater = document.querySelector("in-theater");
+const theater = document.querySelector(".in-theater");
+let tmdbApiKey = "aeaff9aa6a2dfe6e52db45e5c6947320";
+let urlTopTMDB = `https://api.themoviedb.org/3/movie/top_rated?api_key=${tmdbApiKey}`;
+let urlPlayingTMDB = `https://api.themoviedb.org/3/movie/now_playing?api_key=${tmdbApiKey}`;
 
+function top_rated() {
+  fetch(urlTopTMDB)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
 toprated.addEventListener("click", function (event) {
   event.preventDefault();
   window.location = "toprated.html";
