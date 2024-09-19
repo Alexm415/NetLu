074a90472235a5a2c7de177fe392b554f22d2b34
Arman Tork <canadienperse@gmail.com>
2024-09-19 13:10:54 -0700

    semantic and navbar

--- assets/js/toprated.js
+++ assets/js/toprated.js
@@ -1,6 +1,3 @@
-const toprated = document.querySelector(".toprated");
-const featured = document.querySelector(".featured");
-const theater = document.querySelector(".in-theater");
 let containerEl = document.getElementById("container-top");
 let tmdbApiKey = "aeaff9aa6a2dfe6e52db45e5c6947320";
 let urlTopTMDB = `https://api.themoviedb.org/3/movie/top_rated?api_key=${tmdbApiKey}`;
@@ -16,29 +13,16 @@ function top_rated() {
 
       for (let i = 0; i < data.results.length; i++) {
         let movieEl = document.createElement("div");
-        let imgEL = document.createElement("img");
+        // let imgEL = document.createElement("img");
         let releaseDate = document.createElement("h2");
         movieEl.textContent = data.results[i].title;
-        imgEL.src = data.results[i].backdrop_path;
+        // imgEL.src = data.results[i].poster_path;
         releaseDate = data.results[i].release_date;
         console.log(data.results[i].title);
         containerEl.appendChild(movieEl);
-        containerEl.appendChild(imgEL);
+        // containerEl.appendChild(imgEL);
         containerEl.append(releaseDate);
       }
     });
 }
-toprated.addEventListener("click", function (event) {
-  event.preventDefault();
-  window.location = "toprated.html";
-});
-
-featured.addEventListener("click", function (event) {
-  event.preventDefault();
-  window.location = "featured.html";
-});
-theater.addEventListener("click", function (event) {
-  event.preventDefault();
-  window.location = "intheaters.html";
-});
 top_rated();
